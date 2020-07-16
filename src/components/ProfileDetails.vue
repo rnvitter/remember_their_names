4390746762dd4ff3242362371fc1f69104cae8a1
Ryan Vitter <ryanvitter@gmail.com>
2020-07-16 01:54:39 -0400

    another potential css fix for gradient on ios

--- src/components/ProfileDetails.vue
+++ src/components/ProfileDetails.vue
@@ -149,6 +149,7 @@ export default {
   left: 0;
   pointer-events: none;
   background-image: linear-gradient(to bottom, rgba(255,255,255, 0),rgba(34,34,34, 1));
+  background: -webkit-linear-gradient(top,  rgba(255,255,255, 0) 0%,rgba(34,34,34, 1) 100%); /* Chrome10+,Safari5.1+ */
   width: 100%;
   height: 50px;
 }
